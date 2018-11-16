9ba61a9001ecedc9b21ddf81421f9164f79566af
him1411 <him141195@gmail.com>
2018-11-16 12:34:46 +0530

    adding the basic classes with a few functions left to implement

---
+++ basestation.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+class basestation
+{
+    int basestation_id;
+    float locationx,locationy;
+    int no_of_usereqipment,no_of_dms;
+    boolean is_alive;
+    connected_ue = new int[100];
+
+
+    /* to do left 
+        locationofue()
+    */
+}
