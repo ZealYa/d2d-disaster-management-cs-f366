3c3414b1da75755dd5089fff72677248e00287b0
Subhadip Jana <subhadip1997a@gmail.com>
2018-12-03 13:12:10 +0530

    Add BFS based route finder

--- Driver.java
+++ Driver.java
@@ -140,17 +140,109 @@ public class Driver
 					adjList.put(helper, temp1);
 				}
 			}
-			ArrayList<String> routeString = createRouteStrings(helper, adjList.size(), adjList, d.DMSDatabase);
+			ArrayList<String> routeString = createRouteStrings(helper, adjList.size(), adjList, new ArrayList<>(d.DMSDatabase));
+			
 		}
 	}
 	
 	public ArrayList<String> createRouteStrings(UserEquipment helper, int numOfNodes, HashMap<UserEquipment, 
 												ArrayList<UserEquipment>> adjList, ArrayList<UserEquipment> sosUEList)
 	{
-		//Start from low power nodes, find route to helper base-station, the route should have 
+		//Start from low power nodes, find route to helper base-station(UE), the route should have 
 		//less number of middle nodes. Also, no low power device should be in the route.
 		//Mark all visited nodes.
-		return null;
+		Comparator<UserEquipment> UEComparator = new Comparator<UserEquipment>() {
+			
+			@Override
+			public int compare(UserEquipment o1, UserEquipment o2) {
+				// TODO Auto-generated method stub
+				return (int)o1.batteryLevel-(int)o2.batteryLevel;
+			}
+		};
+		
+		ArrayList<String> routeStrings = new ArrayList<>();
+		Collections.sort(sosUEList, UEComparator);
+		
+		for(UserEquipment ue : sosUEList)
+		{	
+			if(ue.isD2DRelay==true) continue;
+			
+			Queue<UserEquipment> q = new LinkedList<>();
+			HashMap<UserEquipment, Boolean> visited = new HashMap<UserEquipment, Boolean>();
+			HashMap<UserEquipment, UserEquipment> pred = new HashMap<UserEquipment, UserEquipment>();
+			pred.put(ue, null);
+			
+			q.add(ue);
+			visited.put(ue, true);
+			int breakFlag = 0;
+			
+			while(!q.isEmpty())
+			{
+				UserEquipment fromNode = q.poll();
+				for(UserEquipment toNode : adjList.get(fromNode))
+				{
+					if((visited.get(toNode)==null || visited.get(toNode)==false) && toNode.batteryLevel>40.0 && toNode.isD2DRelay==false)
+					{
+						visited.put(toNode, true);
+						pred.put(toNode, fromNode);
+						q.add(toNode);
+						
+						if(toNode.equals(helper))
+						{
+							breakFlag=1;
+							break;
+						}
+					}
+				}
+				if(breakFlag==1) 
+							break;
+			}
+			
+			//Print the route string
+			String route = new String("0");
+			UserEquipment node = helper;
+			while(pred.get(node)!=null)
+			{
+				route = route + "|" + pred.get(node).userEquipmentId;
+				//mark d2d in sosList and adjList
+				markD2D(pred.get(node), sosUEList, adjList, 1);
+				node = pred.get(node);
+			}
+			routeStrings.add(route);
+		}
+				
+		return routeStrings;
+	}
+	
+	public boolean markD2D(UserEquipment u, ArrayList<UserEquipment> sosList, HashMap<UserEquipment, ArrayList<UserEquipment>> adjList, int mode)
+	{
+		boolean ret = false;
+		for(UserEquipment ue : sosList)
+		{
+			if(u.userEquipmentId==ue.userEquipmentId) 
+			{
+				ret = ue.isD2DRelay;
+				if(mode==1) ue.isD2DRelay=true;
+			}
+			
+		}
+		
+		for(UserEquipment ueKey : adjList.keySet())
+		{
+			ArrayList<UserEquipment> ueList = adjList.get(ueKey);
+			for(UserEquipment ue : ueList)
+			{
+				if(u.userEquipmentId==ue.userEquipmentId) 
+				{
+					ret = ue.isD2DRelay;
+					if(mode==1) ue.isD2DRelay=true;
+				}
+				
+			}
+		}
+		
+		return ret;
+		
 	}
 	
 	//Create a mesh d2d network
--- MainClass.java
+++ MainClass.java
@@ -4,6 +4,6 @@ public class MainClass
 	public static void main(String[] args) {
 		Driver drvr = new Driver();
 		drvr.buildSimulationScenario();
-		drvr.sendDMSSOS(drvr.DMSList.get(1));
+		drvr.createD2DGraph(drvr.DMSList.get(0));
 	}
 }
